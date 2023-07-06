f1b60da0976d3a75843dc3eaf0eefdba4a884b94
Gatarigospodin <zyrinmaks@gmail.com>
2023-07-06 13:38:54 +0200

    basic server

---
+++ ForkBot/tbd/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TbdConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tbd'
--- main.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-
-url = "https://www.youtube.com/"
-
-with webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())) as driver:
-    driver.get(url)
